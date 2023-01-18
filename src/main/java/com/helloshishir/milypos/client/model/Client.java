4272693294ade5961b24cc9e89ded0bd2d7fe9b4
Iftekhar Hossain <ifte.hsn@gmail.com>
2023-01-19 00:34:42 +0600

    added client model

---
+++ src/main/java/com/helloshishir/milypos/client/model/Client.java
@@ -0,0 +1,49 @@
+package com.helloshishir.milypos.client.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Table(name = "client")
+@Data
+public class Client {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    Integer id;
+
+    @Column(name = "email")
+    String email;
+
+
+    @Column(name = "first_name")
+    String firstName;
+
+    @Column(name = "last_name")
+    String lastName;
+
+    @Column(name = "avatar")
+    String avatar;
+
+
+    @Column(name = "sex")
+    String sex;
+
+    @Column(name = "phone")
+    String phone;
+
+    @Column(name = "fax")
+    String fax;
+
+    @Column(name = "address", columnDefinition = "TEXT")
+    String address;
+
+    @Column(name = "city")
+    String city;
+
+    @Column(name = "state")
+    String state;
+
+    @Column(name = "zip")
+    String zip;
+}
