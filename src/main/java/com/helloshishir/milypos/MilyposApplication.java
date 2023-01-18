94424229f728ac3ce5b6df9ef4450f49369fe351
Iftekhar Hossain <ifte.hsn@gmail.com>
2023-01-18 23:27:18 +0600

    initial commit

---
+++ src/main/java/com/helloshishir/milypos/MilyposApplication.java
@@ -0,0 +1,13 @@
+package com.helloshishir.milypos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MilyposApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MilyposApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/helloshishir/milypos/MilyposApplicationTests.java
@@ -0,0 +1,13 @@
+package com.helloshishir.milypos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MilyposApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
